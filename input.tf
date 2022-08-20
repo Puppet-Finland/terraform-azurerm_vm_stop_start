a300eed41ac335c9d3e41a458d86e8293124efdd
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2022-08-20 10:30:16 +0300

    Allow use of Virtual Machine Contributor role
    This is necessary if you only have "Contributor" role in Azure and can't create
    custom Azure AD roles.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- input.tf
+++ input.tf
@@ -40,6 +40,12 @@ variable "automation_schedule_vm_stop_name" {
   default = "vm-stop"
 }
 
+variable "manage_role_definition" {
+  type        = bool
+  description = "Whether to create and use a restricted role defition for VM start/stop jobs"
+  default     = true
+}
+
 variable "start_job_frequency" {
   type    = string
   default = "Day"
